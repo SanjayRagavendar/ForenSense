47de5a55206bbf8d4602619421ad53953f37eeed
Aldrin Lijo <118544279+aldrinlijo04@users.noreply.github.com>
2024-02-03 15:04:39 +0530

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,30 @@
+#dependencies
+import pandas as pd
+from sklearn.preprocessing  import MinMaxScaler
+import time
+path = "../input/kdd-cup-1999-data/kddcup.data_10_percent.gz"
+columns =
+df = pd.read_csv(path,names=columns)
+def Scaling(df_num, cols):
+    std_scaler = RobustScaler()
+    std_scaler_temp = std_scaler.fit_transform(df_num)
+    std_df = pd.DataFrame(std_scaler_temp, columns =cols)
+    return std_df
+def preprocess(dataframe):
+    df_num = dataframe.drop(cat_cols, axis=1)
+    num_cols = df_num.columns
+    scaled_df = Scaling(df_num, num_cols)
+    
+    dataframe.drop(labels=num_cols, axis="columns", inplace=True)
+    dataframe[num_cols] = scaled_df[num_cols]
+    
+    dataframe.loc[dataframe['outcome'] == "normal", "outcome"] = 0
+    dataframe.loc[dataframe['outcome'] != 0, "outcome"] = 1
+    
+    dataframe = pd.get_dummies(dataframe, columns = ['protocol_type', 'service', 'flag'])
+    return dataframe
+
+
+
+
+
