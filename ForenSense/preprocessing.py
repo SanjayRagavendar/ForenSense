ac22e824114eb3a92c9b7dc28757dd97930ea38f
Aldrin Lijo <aldrinlijo04@gmail.com>
2024-02-04 07:29:00 +0530

    dl

--- ForenSense/preprocessing.py
+++ ForenSense/preprocessing.py
@@ -1,11 +1,15 @@
 #dependencies
-
-import os
 import pandas as pd
-import numpy as np
+import os
 import time
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
-path = "../AdminInterface/output.csv"
+path = "../ForenSense/kddcup.testdata.unlabeled_10_percent.gz"
+path1 = os.path.join('DL','output.csv')
+
+
 cols="""duration,
 protocol_type,
 service,
@@ -52,10 +56,16 @@ columns=[]
 for c in cols.split(','):
     if(c.strip()):
        columns.append(c.strip())
+columns.append('target')
+
+
 df = pd.read_csv(path,names=columns)
+
 num_cols = df._get_numeric_data().columns
 cate_cols = list(set(df.columns)-set(num_cols))
 df = df[[col for col in df if df[col].nunique() > 1]]
+
+
 df.drop('num_root',axis = 1,inplace = True)
 df.drop('srv_serror_rate',axis = 1,inplace = True)
 df.drop('srv_rerror_rate',axis = 1, inplace=True)
@@ -64,26 +74,32 @@ df.drop('dst_host_serror_rate',axis = 1, inplace=True)
 df.drop('dst_host_rerror_rate',axis = 1, inplace=True)
 df.drop('dst_host_srv_rerror_rate',axis = 1, inplace=True)
 df.drop('dst_host_same_srv_rate',axis = 1, inplace=True)
+
 pmap = {'icmp':0,'tcp':1,'udp':2}
 df['protocol_type'] = df['protocol_type'].map(pmap)
 fmap = {'SF':0,'S0':1,'REJ':2,'RSTR':3,'RSTO':4,'SH':5 ,'S1':6 ,'S2':7,'RSTOS0':8,'S3':9 ,'OTH':10}
 df['flag'] = df['flag'].map(fmap)
-df.drop('service',axis = 1,inplace= True)
+df.drop(['service','dst_host_same_src_port_rate'],axis = 1,inplace= True)
+
 
 from sklearn.preprocessing import LabelEncoder
 l=LabelEncoder()
-l.fit_transform(df["Attack Type","protocol_type"])
+l.fit_transform(df["protocol_type"])
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler()
 X = sc.fit_transform(df)
 
-import tensorflow as tf
-from tensorflow.keras.models import load_model
 
-gg = load_model("/kaggle/working/model1.h5")
+
+gg = load_model("model_withattack_type.h5")
 
 start_time = time.time()
 figam=gg.predict(X)
 end_time = time.time()
 
-print(figam.mean())
+label_encoder = LabelEncoder()
+label_encoder.classes_ = np.array(['dos','normal','u2r', 'r2l', 'probe', 'dos'])
+
+predicted_labels = label_encoder.inverse_transform(figam.argmax(axis=1))
+
+print(predicted_labels)
